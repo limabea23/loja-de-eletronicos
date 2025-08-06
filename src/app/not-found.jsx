0768db26ec2e1a6fad7b97948cfa4ce585b7e75e
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-08-06 19:10:41 -0300

    feat: 404 page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 import React from "react";
 import styles from "./not-found.module.css";
+import Image from "next/image";
 
 export default function NotFound() {
   return (
     <div className={styles.container}>
-        <h1 className={styles.title}>404</h1>
-        
-        <p className={styles.message}>Página não encontrada.</p>
-
+        <h1 className={styles.title}> ERRO 404</h1>
+        <p className={styles.message}>A página que você procura não existe.</p>
+        <Image src="/img/loadingif.gif" alt="Gif de carregamento" width={800} height={284} priority className={styles.image} />
         <Link href="/" className={styles.button}>
           Voltar para Home
         </Link>
